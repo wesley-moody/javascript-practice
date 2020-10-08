4be19c4eaa99a46b734164b6b2b35a03c5d4d36f
wesley.moody <wgm3190@gmail.com>
2020-10-08 17:09:33 -0500

    JS objects

--- practice.js
+++ practice.js
@@ -283,19 +283,447 @@
 
 // reusableFunction
 
-// Passing Values to Functions with Arguments
-function ourFunctionWithArgs(a, b) {
-  console.log(a - b);
+//  //Passing Values to Functions with Arguments
+// function ourFunctionWithArgs(a, b) {
+//   console.log(a - b);
+// }
+// ourFunctionWithArgs(10, 5)
+
+// function functionWithArgs(a, b) {
+//   console.log(a + b);
+// }
+// functionWithArgs(10, 5)
+
+// // Global Scope and Functions
+// var myGlobal = 10;
+
+// function fun1() {
+//   oopsGlobal = 5;
+// }
+
+// function fun2() {
+//   var output = '';
+//   if (typeof myGlobal != undefined) {
+//     output += 'myGlobal: ' + oopsGlobal;
+//   }
+//   if (typeof oopsGlobal != 'undefined') {
+//     output += ' oopsglobal: ' + oopsGlobal;
+//   }
+//   console.log(output);
+// }
+// fun1();
+// fun2()
+
+// // Local Scope and Functions
+// function myLocalScope() {
+//   var myVar = 5;
+//   console.log(myVar);
+// }
+// myLocalScope();
+
+// // Global vs. Local Scope in Functions
+// var outerWear = 'T-Shirt';
+
+// function myOutfit() {
+//   var outerWear = 'sweater'
+
+//   return outerWear;
+// }
+
+// console.log(myOutfit());
+// // access local variable by invoking the function
+// console.log(outerWear)
+// // access global variable by calling it by name outside the function
+
+// // Return a Value from a Function with Return
+// function minusSeven(num) {
+//   return num - 7;
+// }
+// console.log(minusSeven(10))
+
+// function timesFive(num) {
+//   return num * 5;
+// }
+// console.log(timesFive(5))
+
+// // Understanding Undefined Value Returned from a Function
+// var sum = 0;
+// function addThree() {
+//   sum = sum + 3;
+// }
+
+// function addFive() {
+//   sum = sum + 5
+//   // -- or --
+//   sum += 5
+// }
+
+// // Assignment with a Returned Value
+// var changed = 0;
+
+// function change(num) {
+//   return (num + 5) / 3;
+// }
+
+// changed = change(10);
+
+// var processed = 0;
+
+// function processedArg(num) {
+//   return (num + 3) / 5;
+// }
+
+// console.log(processedArg(7))
+
+// // Stand in Line
+// function nextInLine(arr, item) {
+//   arr.push(item)
+//   return arr.shift();
+// }
+
+// var testArr = [1,2,3,4,5];
+
+// console.log("Before: " + JSON.stringify(testArr));
+// console.log(nextInLine(testArr, 6));
+// console.log('After: ' + JSON.stringify(testArr));
+
+// // Boolean Values
+// function welcomeToBooleans() {
+//   return true;
+// }
+
+// // Use Conditional Logic with If Statements
+// function ourTrueOrFalse(isItTrue) {
+//   if (isItTrue) {
+//     return "Yes, it's true";
+//   }
+//   return "No, it's false";
+// }
+// function trueOrFalse(wasThatTrue) {
+//   if (wasThatTrue) {
+//     return "Yes, that was true";
+//   }
+//   return "No, that was false";
+// }
+
+// console.log(trueOrFalse(true));
+
+// // Comparison with the Equality Operator
+// function testEqual(val) {
+//   if (val == 12) {
+//     return "Equal";
+//   }
+//   return "Not Equal";
+// }
+// console.log(testEqual(10));
+
+// // Comparison with the Strict Equality Operator
+// function testgStrict(val) {
+//   if (val === 8) {
+//     return "Equal";
+//   }
+//   return "Not Equal";
+// }
+
+// console.log(testgStrict(10));
+
+// // Practice Comparing Different Values
+// function comparEquality(a, b) {
+//   if (a == b) {
+//     return "Equal";
+//   }
+//   return "Not Equal";
+// }
+
+// console.log(comparEquality(10, "10"))
+
+// // Comparison with the Inequality Operator
+// function testNotEqual(val) {
+//   if (val != 99) {
+//     return "Not Equal";
+//   }
+//   return "Equal";
+// }
+// console.log(testNotEqual(10))
+
+// // Comparison with the Strict Inequality operator
+// function testStrictNotEqual(val) {
+//   if (val !== 17) {
+//     return "Not Equal";
+//   }
+//   return "Equal"
+// }
+// console.log(testStrictNotEqual(10))
+
+// // Comparisons with the Logical And Operator
+// function testGreaterThan(val) {
+//   if (val > 100) {
+//     return "Over 100";
+//   }
+//   if (val > 10) {
+//     return "Over 10"
+//   }
+//   return "10 or Under"
+// }
+
+// console.log(testGreaterThan(10))
+
+// // Comparison with the Greater Than or Equal To Operator
+// function testGreaterOrEqual(val) {
+//   if(val >= 20) {
+//     return "20 or over";
+//   }
+//   if (val >= 10) {
+//     return "10 or over";
+//   }
+//   return "Less than 10";
+// }
+
+// console.log(testGreaterOrEqual(10))
+
+// // Else Statements
+// function testElse(val) {
+//   var result = "";
+//   if (val > 5) {
+//     result = "Bigger than 5";
+//   } else {
+//     result = "5 or smaller"
+//   }
+// return result;
+// }
+
+// console.log(testElse(4))
+
+// // Else If Statements
+// function testElseIf(val) {
+//   if (val > 10) {
+//     return "Greater than 10";
+//   } else if (val < 5) {
+//     return "Smaller than 5";
+//   } else {
+//     return "Between 5 and 10"
+//   }
+// }
+
+// console.log(testElseIf(7));
+
+// Logical Order in If Else Statements
+// function orderMyLogic(val) {
+//   if (val < 5) {
+//     return "Less than 5"
+//   } else if (val < 10) {
+//     return "Less than 10";
+//   } else {
+//     return "Greater than or equal to 10"
+//   }
+  
+// }
+
+// console.log(orderMyLogic(4));
+// console.log(orderMyLogic(7));
+// console.log(orderMyLogic(19));
+
+// // Chaining If/ Else if statements
+// function testSize(num) {
+//   if (num < 5) {
+//     return "Tiny"
+//   } else if (num < 10) {
+//     return "Small"
+//   } else if (num < 15) {
+//     return "Medium"
+//   } else if (num < 20) {
+//     return "Large"
+//   } else {
+//     return "Huge"
+//   }
+// }
+
+// console.log(testSize(4))
+// console.log(testSize(7))
+// console.log(testSize(12))
+// console.log(testSize(19))
+// console.log(testSize(44))
+
+// // Golf Code
+// var names = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "You're drunk, go home"]
+// function golfScore(par, strokes) {
+//   if (strokes == 1) {
+//     return names[0]
+//   } else if (strokes <= par - 2) {
+//     return names[1]
+//   } else if (strokes == par - 1) {
+//     return names[2] 
+//   } else if (strokes == par) {
+//     return names[3] 
+//   } else if (strokes == par + 1) {
+//     return names[4] 
+//   } else if (strokes == par + 2) {
+//     return names[5] 
+//   } else {
+//     return names[6]
+//   }
+// }
+
+// console.log(golfScore(5, 5))
+
+// // Switch Statements
+// function caseInSwitch(val) {
+//   var answer = "";
+//   switch(val) {
+//     case 1:  
+//       answer = "alpha";
+//       break;
+//     case 2:
+//       answer = "beta";
+//       break
+//     case 3:
+//       answer = "gamma"
+//       break
+//     case 4:
+//       answer = "delta"
+//       break;
+//   }
+//   return answer
+// }
+// console.log(caseInSwitch(1));
+// console.log(caseInSwitch(2));
+// console.log(caseInSwitch(3));
+// console.log(caseInSwitch(4));
+
+// // Default Option in Switch Statements
+// function switchOfStuff(val) {
+//   var answer = "";
+//   switch (val) {
+//     case "a":
+//       answer = "apple";
+//       break;
+//     case "b":
+//       answer = "bird";
+//       break;  
+//     case "c":
+//       answer = "cat";
+//       break;
+//     default:
+//       answer = "stuff";
+//       break
+//   }
+//     return answer
+// }
+
+// console.log(switchOfStuff(13))
+
+// // Multiple identical Options in Switch Statements
+// function sequentialSizes(val) {
+//   var answer = "";
+//   switch(val) {
+//     case 1:
+//     case 2:
+//     case 3:
+//       answer = "Low"
+//       break;
+//     case 4:
+//     case 5:
+//     case 6:
+//       answer = "Middle"
+//       break;
+//     case 7:
+//     case 8:
+//     case 9:
+//       answer = "High"
+//       break;
+//     default:
+//       answer = "Go home, you're drunk";
+//       break;
+//   }
+//   return answer; 
+// }
+
+// console.log(sequentialSizes(33))
+
+// // Replacing If Else Chains with Switch
+// function chainToSwitch(val) {
+//   var answer = "";
+//   switch(val) {
+//     case "bob":
+//       answer = "Marley";
+//       break
+//     case 42:
+//       answer = "Jackie Robinson";      
+//       break;
+//     case 1:
+//       answer = "There is no #1";
+//       break;
+//     case 99:
+//       answer = "Just a bit outside!";
+//       break;
+//     case 7:
+//       answer = "Ate Nine"
+//       break;
+//     default:
+//       answer = "You're killing me, Smalls!";
+//       break;
+//   }
+//   return answer;
+// }
+// console.log(chainToSwitch(99))
+
+// // Returning Boolean Values from Functions
+// function isLess(a, b) {
+//   return a < b;
+//   // if (a < b) {
+//   //   return true;
+//   // } else {
+//   //   return false
+//   // }
+// }
+
+// console.log(isLess(22, 4))
+
+// // Returning Early Pattern from Functions
+// function abTest(a, b) {
+//   if (a , 0 || b < 0) {
+//     return undefined
+//   }
+//   return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
+// }
+
+// console.log(abTest(-2, 2))
+
+// Counting Cards
+var count = 0
+function cc(card) {
+  switch(card) {
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+    case 6:
+      count++;
+      break;
+    case 10:
+    case "J":
+    case "Q":
+    case "K":
+    case "A":
+      count --;
+      break;
+  }
+  var holdbet = 'Hold'
+  if (count > 0) {
+    holdbet = 'Bet'
+  }
+
+  return count + " " + holdbet;
 }
-ourFunctionWithArgs(10, 5)
+cc(2); cc(3); cc(4); cc('K'); cc('A');
+console.log(cc(3))
 
+// Build JS Objects
+var ourDog = {
+  "name": "Paige",
+  "legs": 3,
+  "tails": 2,
+  "friends": []
+}
 
-
-
-
-
-
-
-
-
-
+//
