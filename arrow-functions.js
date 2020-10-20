88da8bdc1326e5a42503f5a314b351f1cf5e6503
wesley.moody <wgm3190@gmail.com>
2020-10-20 09:23:14 -0500

    saving practice

--- arrow-functions.js
+++ arrow-functions.js
@@ -13,7 +13,7 @@
 
 //Write Arrow Functions with Parameters
 var myConcat = function(arr1, arr2) {
-  return arr1.concat(arr2);
+  return arr1.concat(arr2); 
 };
 console.log(myConcat([1, 2], [3, 4, 5]))
 
